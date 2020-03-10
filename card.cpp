e8b9a2cb71edb8a93b40f481cb31f602f5f8116c
slyni <slyni@naver.com>
2020-03-10 13:29:07 +0900

    EFFECT_SUMMONABLE_CARD

--- card.cpp
+++ card.cpp
@@ -2989,11 +2989,13 @@ int32 card::check_cost_condition(int32 ecode, int32 playerid, int32 sumtype) {
 	return TRUE;
 }
 int32 card::is_summonable_card() {
-	if(!(data.type & TYPE_MONSTER))
+	if (!(data.type & TYPE_MONSTER))
 		return FALSE;
 	effect_set eset;
-	filter_effect(EFFECT_REVIVE_LIMIT, &eset);
-	return (eset.size() < 1) && !(data.type & (TYPE_SPSUMMON | TYPE_TOKEN));
+	filter_effect(EFFECT_SUMMONABLE_CARD, &eset);
+	return (eset.size() > 0) || !(data.type & (TYPE_RITUAL | TYPE_SPSUMMON
+		| TYPE_FUSION | TYPE_SYNCHRO | TYPE_XYZ | TYPE_LINK 
+		| TYPE_TOKEN));
 }
 int32 card::is_fusion_summonable_card(uint32 summon_type) {
 	if(!(data.type & TYPE_FUSION))
