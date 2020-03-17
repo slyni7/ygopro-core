0b0f5c16bdf9e6a53af0f31dbcc26a6ee8e520bf
slyni <slyni@naver.com>
2020-03-17 22:23:30 +0900

    fix

--- interpreter.cpp
+++ interpreter.cpp
@@ -152,7 +152,7 @@ int32 interpreter::register_card(card *pcard) {
 	lua_setmetatable(current_state, -2);
 	lua_pop(current_state, 1);
 	//Initial
-	if(pcard->data.code && (!(pcard->data.type & TYPE_NORMAL) || (pcard->data.type & TYPE_PENDULUM))) {
+	if(pcard->data.code && (!(pcard->data.type & TYPE_NORMAL) || (pcard->data.type & TYPE_PENDULUM + TYPE_SQUARE))) {
 		pcard->set_status(STATUS_INITIALIZING, TRUE);
 		add_param(pcard, PARAM_TYPE_CARD);
 		call_card_function(pcard, "initial_effect", 1, 0);
